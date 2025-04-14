7b8bb6d60312b7758ed8965b7e29a7fee7555506
Julia Zanirato <juliazaniratoo@gmail.com>
2025-04-14 09:25:15 -0300

    Update perfil_adm.php

--- perfil_adm.php
+++ perfil_adm.php
@@ -104,7 +104,47 @@
                             </label>
                             <input type="number" name="dias_calculo" class="form-control" value="1" required>
                         </div>
+                        <button class="btn btn-success w-100" type="submit" name="calcular"> Calcular previsão </button>
                     </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Tabela  de veiculos cadastrados -->
+    <div class="row mt-4">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="mb-0">
+                        Veículos cadastrados🗒️
+                    </h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead>
+                                <th>Tipo</th>
+                                <th>Modelo</th>
+                                <th>Placa</th>
+                                <th>Status</th>
+                                <th>Ações</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Carro</td>
+                                    <td>Uno</td>
+                                    <td>ABC1D34</td>
+                                    <td>
+                                        <span class="badge bg-success"> DIsponível ✅</span>
+                                    </td>
+                                    <td> 
+                                        <div class="action-wrapper"></div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
